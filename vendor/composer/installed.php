d4f9e88e2428b514cdf5d436dfa0b4c15e32650a
Markus Staab <markus.staab@redaxo.de>
2020-11-22 12:23:31 +0100

    added splitbrain/php-archive

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '51910207df68ebba098f96a1173e2e01f6605215',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '51910207df68ebba098f96a1173e2e01f6605215',
+    ),
+    'splitbrain/php-archive' => 
+    array (
+      'pretty_version' => '1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd4cf2d9a2d82548b7e4f1dc04802c526eba68b65',
+    ),
+  ),
+);
